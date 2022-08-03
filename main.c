e55b3fe7768719751ac9ab6290cf33211a526e0e
Henjie Agadan <70084057+hagdn@users.noreply.github.com>
2022-08-03 20:25:34 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+ISR(PCINT0_vect) {
+  if (PINB & 0b00000001) {
+    PORTB |= (1 << PORTB2); //Button to PIN8 IS pressed
+  } else if (!(PINB & 0b00000001)) {
+    PORTB &= ~(1 << PORTB2); // Button to PIN8 NOT pressed
+  }
+
+  if (PINB & 0b00000010) {
+    PORTB |= (1 << PORTB5); // Button to PIN9 IS pressed
+  } else if (!(PINB & 0b00000010)) {
+    PORTB &= ~(1 << PORTB5); // Button to Pin9 NOT pressed
+  }
+}
+
+int main(void) {
+  PCICR |= (1 << 0); // Activate Group 0 pin interrupts (PIN8 - PIN13)
+  PCMSK0 |= (1 << PCINT0) | (1 << PCINT1); // Set PIN8 & PIN9 as interrupts
+
+  //PORTB |=(1 << PORTB0) | (1 << PORTB1); // Pullup resistor for PIN8 & PIN9
+
+  DDRB |= (1 << DDB2) | (1 << DDB5); // Set PIN10 & PIN13 as OUTPUT pins
+
+  sei(); // Activate global interrupts
+
+  while (1) {
+    //...
+  }
+}
